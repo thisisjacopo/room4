99e080e3c1868b51ce194757020c4e151e935ae3
Jacopo <jacopo.rodighiero@icloud.com>
2022-09-28 18:09:13 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+gsap.fromTo(
+    ".logo",
+    { x: 0, y: -600 },
+    { x: 0, y: 0, duration: 1 }
+  );
+  gsap.fromTo(
+    ".text",
+    { x: -900, y: 0 },
+    { x: 0, y: 0, duration: 1 }
+  );
